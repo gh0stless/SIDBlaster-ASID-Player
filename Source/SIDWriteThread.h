10f25b9b89b384521dbfbb3711a2c9ceb6bb962b
Andreas Schumm <schumm@arcor.de>
2024-10-29 21:20:09 +0100

    Super

--- Source/SIDWriteThread.h
+++ Source/SIDWriteThread.h
@@ -24,40 +24,31 @@ public:
             bool cie = false;
             cie = ringBuffer.remove(value);
                 if (cie) {
-                    int buffersize = 0;
-                    buffersize = ringBuffer.size();
-
-                    if (buffersize == 0 || cycles == WRITES_PER_FRAME - 8) {
-                        RS = 0;
-                        while (RS != HSID_USB_WSTATE_OK) { //letzter write
-                            RS = HardSID_Try_Write(0, FRAME_IN_CYCLES - cycles, value.SIDRegister, value.SIDData);
-                            if (RS == HSID_USB_WSTATE_BUSY) juce::Thread::sleep(20);
-                        }
-                        cycles = 0;
-                    }
-                    else {
-                        RS = 0;
-                        while (RS != HSID_USB_WSTATE_OK) {//normaler write
-                            RS = HardSID_Try_Write(0, cycles, value.SIDRegister, value.SIDData);
-                            if (RS == HSID_USB_WSTATE_BUSY) juce::Thread::sleep(20);
-                        }
-                        cycles = cycles+8;
-                    }
-            }
-            else { // wenn der Puffer leer ist
-                RS = 0;
-                while (RS != HSID_USB_WSTATE_OK) {
-                    RS = HardSID_Try_Write(0, FRAME_IN_CYCLES, 0x1e, 0);  // mache nichts
-                    if (RS == HSID_USB_WSTATE_BUSY) juce::Thread::sleep(20);
+                    HardSID_WriteWithTimeout(0, cycles, value.SIDRegister, value.SIDData);
                 }
-                juce::Thread::sleep(20);
-            }
-                
+                else { // wenn der Puffer leer ist
+                    HardSID_WriteWithTimeout(0, FRAME_IN_CYCLES, value.SIDRegister, value.SIDData);
+                }
         }
     }
 
+    bool HardSID_WriteWithTimeout(int dev_id, int cycles, int reg, int data) {
+        auto startTime = juce::Time::getMillisecondCounter();
+
+        while (HardSID_Try_Write(dev_id, cycles, reg, data) == HSID_USB_WSTATE_BUSY) {
+            juce::Thread::yield();  // CPU-Zeit für andere Threads freigeben
+
+            auto elapsedTime = juce::Time::getMillisecondCounter() - startTime;
+            if (elapsedTime > LOOP_TIME_OUT_MILLIS) {
+                return false;  // Timeout erreicht, Rückgabe false
+            }
+        }
+        return true;  // Schreibvorgang erfolgreich
+    }
+
 private:
+    int LOOP_TIME_OUT_MILLIS = 500;  // Timeout in Millisekunden
     ThreadSafeRingBuffer<SIDWriteSet>& ringBuffer;
-    int cycles = 0;
+    int cycles = 8;
     SIDWriteSet value;
 };
